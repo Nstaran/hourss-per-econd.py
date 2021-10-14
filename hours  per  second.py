22488304130f9173ef3209c60559811a821fd20b
Nstaran <91914646+Nstaran@users.noreply.github.com>
2021-10-14 03:50:42 -0700

    Add files via upload

---
+++ hours  per  second.py
@@ -0,0 +1,8 @@
+print("enter the hour")
+hour=int(input())
+print("enter the minute")
+minute=int(input())
+print("enter the seconds")
+seconds=int(input())
+sum=hour*3600+minute*60+seconds
+print(sum)
